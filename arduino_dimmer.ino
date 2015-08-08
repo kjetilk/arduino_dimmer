426c52d5abe03431e0eeacb93c0a8a4b2fedef01
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2015-08-09 01:20:38 +0200

    Really initial dimmer code

--- arduino_dimmer.ino
+++ arduino_dimmer.ino
@@ -1,24 +1,68 @@
 /*
-
-
+ This is the code I use to control the dimmers in my house.
+ 
+ The LEDs are driven by an Eltako LUD12, that takes a PWM signal with an amplitude of 10-24V, 
+ and pulse width 0-90% to set the light level.
+ It runs on a Mega where pins 2-11 except 4 and 10 (which is needed by an Ethernet controller (future project))
+ are connected with some NPN transistors and some resistors, to the X2 connector on the LUD12.
+ 
+ Based on the PWM.h example. Consider this code released under CC0.
+ 
+ Kjetil Kjernsmo <kjetil@kjernsmo.net>
  */
 
 #include <PWM.h>
 
-//use pin 11 on the Mega instead, otherwise there is a frequency cap at 31 Hz
-int led = 9;                // the pin that the LED is attached to
-int brightness = 0;         // how bright the LED is
-int fadeAmount = 5;         // how many points to fade the LED by
-int32_t frequency = 100; //frequency (in Hz)
+
+int leds[] = {
+  2,
+  3,
+  5,
+  6,
+  7,
+  8,
+  9,
+  11
+};
+
+byte upinputs[] = { 
+  22,
+  24,
+  26,
+  28,
+  30,
+  32,
+  34,
+  36
+};
+
+byte downinputs[] = { 
+  21,
+  23,
+  25,
+  27,
+  29,
+  31,
+  33,
+  35
+};
+
+unsigned int brightness[8] = {0};    // how bright the LEDs are
+int fadeAmount = 5;         // how many points to fade a LED by
+int32_t frequency = 100;    // frequency (in Hz)
 
 void setup()
 {
   //initialize all timers except for 0, to save time keeping functions
   InitTimersSafe(); 
 
-  //sets the frequency for the specified pin
-  bool success = SetPinFrequencySafe(led, frequency);
+  //sets the frequency for the specified pins
+  bool success = false;
   
+  for( unsigned int i = 0; i < sizeof( leds ); ++i ) {
+    success = SetPinFrequencySafe(leds[i], frequency);
+    // TODO pack successes to a byte to set success if all are true 
+  }
   //if the pin frequency was set successfully, turn pin 13 on
   if(success) {
     pinMode(13, OUTPUT);
@@ -28,15 +72,18 @@ void setup()
 
 void loop()
 {
-  //use this functions instead of analogWrite on 'initialized' pins
-  pwmWrite(led, brightness);
+  for( unsigned int i = 0; i < sizeof( leds ); ++i ) {
+    if( digitalRead( upinputs[ i ] ) == 0 ) { // Up button pressed
+      brightness[i] = brightness[i] + fadeAmount;
+    }  
+    if( digitalRead( downinputs[ i ] ) == 0 ) { // Down button pressed
+      brightness[i] = brightness[i] - fadeAmount;
+    }  
+    
+    // Write the state to the LUD
+    pwmWrite(leds[i], brightness[i]);
 
-  brightness = brightness + fadeAmount;
-
-  if (brightness == 0 || brightness == 255) {
-    fadeAmount = -fadeAmount ; 
-  }     
-  
-  delay(30);      
+    // TODO: Deal with wrapparound?
+  } 
 }
 
